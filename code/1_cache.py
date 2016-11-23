77538613bbc347ab9374f731b4581008f0c3c51f
Mouaad Aallam <aallam.mouaad@gmail.com>
2016-11-23 13:13:58 +0100

    Add cumulative distribution of number of songs plays

--- code/1_cache.py
+++ code/1_cache.py
@@ -4,18 +4,18 @@ plays_header = ['userID','songID','plays']
 songs2tracks_header = ['songID', 'trackID']
 tracks_header = ['trackID','songID','artist','title']
 
-plays_df = pd.read_csv('../csv/train_triplets.txt', sep='\t', names=plays_header)
-print "[LOADED] train_triplets.txt"
-songs2tracks_df = pd.read_csv('../csv/song_to_tracks.txt', sep='\t', names=songs2tracks_header)
-print "[LOADED] songs_to_tracks.txt"
-tracks_df = pd.read_csv('../csv/unique_tracks.txt', sep='\t', names=tracks_header)
-print "[LOADED] unique_tracks.txt"
+plays_df = pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
+print "[LOADED] train_triplets"
+songs2tracks_df = pd.read_csv('../csv/song_to_tracks', sep='\t', names=songs2tracks_header)
+print "[LOADED] songs_to_tracks.csv"
+tracks_df = pd.read_csv('../csv/unique_tracks.csv', sep='\t', names=tracks_header)
+print "[LOADED] unique_tracks"
 n_entries = plays_df.count()[0]
 n_tracks = tracks_df.count()[0]
 n_users = plays_df.userID.unique().shape[0]
 n_songs = plays_df.songID.unique().shape[0]
 
-print '\nAnalysis:' 
+print '\nAnalysis:'
 print '- Number of unique tracks  = ' + str(n_tracks)
 print '- Number of plays entries  = ' + str(n_entries)
 print '- Number of users = '+str(n_users)
--- code/2_analysis.py
+++ code/2_analysis.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-print "[LOADING] train_triplets.txt..."
+print "[LOADING] train_triplets..."
 plays_header = ['userID','songID','plays']
-plays_df = pd.read_csv('../csv/train_triplets.txt', sep='\t', names=plays_header)
+plays_df = pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
 
 # Total entries
 total_entries = plays_df.count()[0]
@@ -19,10 +20,26 @@ n = len(number_listens)
 x = range(n) 
 width = 1/1.5
 plt.bar(x, number_listens, width, color="blue")
-plt.xlabel("Plays")
-plt.ylabel("Percentage")
-plt.grid()
+plt.xlabel("Plays"); plt.ylabel("%")
+plt.title("Percentage number of plays of songs")
+plt.grid(b=True, which='major', color='k', linestyle='-')
+plt.grid(b=True, which='minor', color='r', linestyle='-', alpha=0.2)
+plt.minorticks_on()
 plt.savefig("../img/percentage_song_plays.png")
 print "[SAVED] percentage_song_plays.png"
-plt.show()
+#plt.show()
+plt.clf()
 
+# Cumulative sum
+cumsum_listens = np.cumsum(number_listens)
+cumsum_listens = np.insert(cumsum_listens, 0, 0)
+plt.plot(np.linspace(0,10,11),cumsum_listens)
+plt.xticks(np.linspace(0,10,11), np.linspace(0,10,11))
+plt.xlabel("Number of Plays"); plt.ylabel("Cumulative sum")
+plt.title("Cumulative disribution of Number of Song Plays")
+plt.grid(b=True, which='major', color='k', linestyle='-')
+plt.grid(b=True, which='minor', color='r', linestyle='-', alpha=0.2)
+plt.minorticks_on()
+plt.savefig("../img/cumulative_sum.png")
+print "[SAVED] cumulative_sum.png"
+#plt.show()
