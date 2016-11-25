77c28b1d3dd4a0c2467c1b66450e2dc93d09c0bd
Mouaad Aallam <Mouaad@Aallam.com>
2016-11-25 19:05:02 +0100

    prettier print

--- code/3_basic.py
+++ code/3_basic.py
@@ -12,10 +12,10 @@ tracks_df = pd.read_csv('../csv/unique_tracks.csv', sep='\t', names=tracks_heade
 # Get the most listened songs
 songs_total_listens = plays_df.groupby('songID')['plays'].sum().sort_values(ascending=False).to_frame()
 print "\n# Top songs with most total listens:"
-print songs_total_listens.head()
+print songs_total_listens.head().to_string()
 
 
 # Join songs with tracks
 tracks_total_listens = songs_total_listens.join(tracks_df.set_index('songID')).sort_values('plays', ascending = False)
 print "\n# Top tracks with most total lisens:"
-print tracks_total_listens.head()
+print tracks_total_listens.head().to_string()
