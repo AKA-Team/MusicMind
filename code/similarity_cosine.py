e834a852d2c163d9ba9dbca9310594153f0c55b0
KEITA chegou junior <keita@informatique.univ-paris-diderot.fr>
2016-12-06 21:38:04 +0100

    similarity_cosinus/similarity_pearson

---
+++ code/similarity_cosine.py
@@ -0,0 +1,25 @@
+import graphlab as gl
+from graphlab import SFrame as sf
+import pandas as pd
+
+# Load datasets
+
+plays_header = ['user_id','item_id','rating']
+
+print "[LOADING] train_triplets"
+
+df= pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
+
+data=sf(data=df)
+
+model = gl.item_similarity_recommender.create(data, target="rating",similarity_type='cosine')
+
+prediction= model.predict(data);
+
+recommendation= model.recommend();
+
+recommendation.save('../similarities')
+
+print prediction
+
+print recommendation
