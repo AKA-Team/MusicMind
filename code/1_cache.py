dc600c0b541cc1682f9fd28855e6281ecd6fc077
Mouaad Aallam <Mouaad@Aallam.com>
2016-12-06 21:42:25 +0100

    Update to lastest dataset

--- code/1_cache.py
+++ code/1_cache.py
@@ -1,23 +1,19 @@
 import pandas as pd
 
 plays_header = ['userID','songID','plays']
-songs2tracks_header = ['songID', 'trackID']
-tracks_header = ['trackID','songID','artist','title']
 
 print "[LOADING] train_triplets"
 plays_df = pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
-print "[LOADING] songs_to_tracks"
-songs2tracks_df = pd.read_csv('../csv/song_to_tracks.csv', sep='\t', names=songs2tracks_header)
-print "[LOADING] unique_tracks"
-tracks_df = pd.read_csv('../csv/unique_tracks.csv', sep='\t', names=tracks_header)
+print "[LOADING] Songs Data"
+songs_df = pd.read_csv('../csv/unique_tracks.csv', sep=',', header=0)
 
 n_entries = plays_df.count()[0]
-n_tracks = tracks_df.count()[0]
+n_tracks = songs_df.count()[0]
 n_users = plays_df.userID.unique().shape[0]
 n_songs = plays_df.songID.unique().shape[0]
 
 print '\nAnalysis:'
-print '- Number of unique tracks  = ' + str(n_tracks)
+print '- Number of unique songs  = ' + str(n_tracks)
 print '- Number of plays entries  = ' + str(n_entries)
 print '- Number of users = '+str(n_users)
-print '- Number of songs = '+str(n_songs)
+print '- Number of songs listened to = '+str(n_songs)
--- code/3_basic.py
+++ code/3_basic.py
@@ -9,9 +9,8 @@ print "[LOADING] train_triplets..."
 plays_header = ['userID','songID','plays']
 plays_df = pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
 
-print "[LOADING] unique_tracks"
-tracks_header = ['trackID','songID','artist','title']
-tracks_df = pd.read_csv('../csv/unique_tracks.csv', sep='\t', names=tracks_header)
+print "[LOADING] Song Data"
+songs_df = pd.read_csv('../csv/song_data.csv', sep=',', header=0)
 
 # Get the most listened songs
 songs_total_listens = plays_df.groupby('songID')['plays'].sum().sort_values(ascending=False).to_frame()
@@ -19,7 +18,7 @@ print "\n# Top songs with most total listens:"
 print songs_total_listens.head().to_string()
 
 
-# Join songs with tracks
-tracks_total_listens = songs_total_listens.join(tracks_df.set_index('songID')).sort_values('plays', ascending = False)
-print "\n# Top tracks with most total lisens:"
-print tracks_total_listens.head().to_string().encode('utf-8')
+# Join songs with data
+songs_total_listens = songs_total_listens.join(songs_df.set_index('songID')).sort_values('plays', ascending = False)
+print "\n# Top Songs with most total lisens:"
+print songs_total_listens.head().to_string().encode('utf-8')
