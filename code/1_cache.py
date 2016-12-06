63396eeeaf090c5e3555afc0854eaea46f1a6f40
Mouaad Aallam <Mouaad@Aallam.com>
2016-12-06 21:47:47 +0100

    updating to new file

--- code/1_cache.py
+++ code/1_cache.py
@@ -5,7 +5,7 @@ plays_header = ['userID','songID','plays']
 print "[LOADING] train_triplets"
 plays_df = pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
 print "[LOADING] Songs Data"
-songs_df = pd.read_csv('../csv/unique_tracks.csv', sep=',', header=0)
+songs_df = pd.read_csv('../csv/song_data.csv', sep=',', header=0)
 
 n_entries = plays_df.count()[0]
 n_tracks = songs_df.count()[0]
