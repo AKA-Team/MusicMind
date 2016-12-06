00d4fa6ef9894ee717cfbcadb41f24ba065ba06c
eric <eric.uzenat@gmail.com>
2016-12-06 16:05:06 +0100

    update matrix add similarity_user/content

---
+++ code/similarity_user.py
@@ -0,0 +1,23 @@
+import graphlab as gl
+from graphlab import SFrame as sf
+import pandas as pd
+
+# Load datasets
+
+plays_header = ['user_id','item_id','rating']
+
+print "[LOADING] train_triplets"
+
+df= pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
+
+data=sf(data=df)
+
+model = gl.item_similarity_recommender.create(data, target="rating",similarity_type='pearson')
+
+prediction= model.predict(dsf);
+
+recommendation= model.recommend();
+
+recommendation.save('../similarities')
+
+print recommendation
