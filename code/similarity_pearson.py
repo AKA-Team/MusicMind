314e976c1f68d638ccfd8c769534ba63c4e8a2e7
KEITA chegou junior <keita@informatique.univ-paris-diderot.fr>
2016-12-13 23:23:33 +0100

    update pearson

--- code/similarity_pearson.py
+++ code/similarity_pearson.py
@@ -1,27 +1,31 @@
 import graphlab as gl
+import sqlite3
 from graphlab import SFrame as sf
-import pandas as pd
 
-# Load datasets
+# Load dataset
+print "[LOADING] data"
+conn = sqlite3.connect('../sqlite/msd.sqlite3')
+data= gl.SFrame.from_sql(conn, "SELECT * FROM train where plays")
 
-plays_header = ['user_id','item_id','rating']
+# Create Training set and test set
+train_data, test_data = gl.recommender.util.random_split_by_user(data, 'userID', 'songID')
 
-print "[LOADING] train_triplets"
+# Create the model
+model = gl.item_similarity_recommender.create(train_data, 'userID', 'songID', similarity_type='pearson')
 
-df= pd.read_csv('../csv/train_triplets.csv', sep='\t', names=plays_header)
+# Recommend to users
+recom = model.recommend()
 
-data=sf(data=df)
+# Evaluate the model
+rmse_data = model.evaluate_rmse(test_data, target='plays')
 
-model = gl.item_similarity_recommender.create(data, target="rating",similarity_type='pearson')
+# Show results
+recom.show()
 
-prediction= model.predict(data);
+# Print result
+print recom
 
-#evaluation = gl.item_similarity_recommender.
+# Print evaluation
+print rmse_data
 
-recommendation= model.recommend();
-
-recommendation.save('../similarities')
-
-
-
-print recommendation
+raw_input("...")
